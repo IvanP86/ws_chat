23307648bf83b344debb72587ca7e2f6a2983785
IvanP86 <inthedark86@yandex.ru>
2024-03-20 14:14:13 +0500

    migrations, models, chats vue page

---
+++ resources/js/Pages/Chat/Index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex">
+        <div class="w-1/2 border border-gray-600">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates consectetur suscipit temporibus hic, veritatis saepe nulla ullam obcaecati accusamus at officiis expedita natus sit laudantium architecto ex consequatur molestiae numquam.
+
+        </div>
+        <div class="w-1/2 border border-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quidem neque doloremque maiores eius veritatis accusantium dolorem ullam facere magnam commodi omnis, consequatur impedit aperiam distinctio quasi, expedita enim minus?
+        </div>
+    </div>
+</template>
+
+<script>
+import Main from "@/Layouts/Main.vue"
+export default {
+    name: 'Index',
+    layout: Main
+}
+</script>
+
+<style scoped>
+
+</style>
